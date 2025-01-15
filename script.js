a850498a081689c670de46cc828a3fb531f82097
Hannes <142382297+waechtler-hannes@users.noreply.github.com>
2025-01-15 07:24:17 +0100

    Add functions to make the game work
    Added getBoard-function to return the board to the GameController.
    Added getCellValue-function to return the values of a single cell to
    the GameController.
    Added getWinPatterns-function that returns 8 arrays (for the 8
    win-patterns) from the board.
    Added switchActivePlayer-function to switch the player after one turn.
    Added checkWin-function that checks all win-patterns, if one of them
    has 3 identical markers, the player with this marker wins, if the whole
    board is filled with markers, but no pattern gets triggered, its a tie.
    Edited playRound-function:
        1. get User-input
        2. check if the picked cell is available
        3. if available, place marker
              check if the player has won or if its a tie
              switch the active player
              play a new round
        4. if not available, replay the round without changing players

--- script.js
+++ script.js
@@ -10,11 +10,30 @@ function Gameboard() {
         }
     }
 
+    function getBoard() {
+        return board;
+    }
+
     function placeMarker(cell, activePlayer) {
         board[cell.row][cell.column] = activePlayer.marker;
     }
 
-    return {placeMarker};
+    function getCellValue(cell) {
+        return board[cell.row][cell.column];
+    }
+
+    function getWinPatterns() {
+        return [[board[0][0], board[0][1], board[0][2]], 
+                [board[1][0], board[1][1], board[1][2]],
+                [board[2][0], board[2][1], board[2][2]],
+                [board[0][0], board[1][0], board[2][0]],
+                [board[0][1], board[1][1], board[2][1]],
+                [board[0][2], board[1][2], board[2][2]],
+                [board[0][0], board[1][1], board[2][2]],
+                [board[0][2], board[1][1], board[2][0]]];
+    }
+
+    return {getBoard, placeMarker, getCellValue, getWinPatterns};
 }
 
 function GameController(playerOneName = "Player 1", playerTwoName = "Player 2") {
@@ -27,16 +46,37 @@ function GameController(playerOneName = "Player 1", playerTwoName = "Player 2") 
 
     let activePlayer = players[0];
 
+    function switchActivePlayer() {
+        activePlayer = (activePlayer === players[0]) ? players[1] : players[0];
+    }
+
     function getPlayerInput() {
         return {
             row: prompt("Select row:"), 
             column: prompt("Select column:")
         };
     }
+
+    function checkWin(activePlayer) {
+        for(let i = 0; board.getWinPatterns().length > i; i++) {
+            if (board.getWinPatterns()[i].every((cell) => cell === activePlayer.marker)) {
+                console.log(`${activePlayer.name} wins.`)
+            }
+        }
+        if (board.getBoard().flat().every((cell) => cell)) {
+            console.log("Tie");
+        }
+    }
     
     function playRound() {
         const cell = getPlayerInput();
-        board.placeMarker(cell, activePlayer);
+        if (board.getCellValue(cell) === "") {
+            board.placeMarker(cell, activePlayer);
+            checkWin(activePlayer);
+            switchActivePlayer();
+        }
+        console.log(board.getBoard());
+        playRound();
     }
 
     playRound();

839cd58e6c8b61ba438bb751a737a5d889f8f99d
Hannes <142382297+waechtler-hannes@users.noreply.github.com>
2025-01-22 12:49:38 +0100

    Add logic to display the markers on the board.
    Changed "updateBoard"-function to display the x- and o-svg's on the
    board and delete them if a new game starts.
    Changed "updateMessagePanel"-function to display the x- and o-svg's on
    it.
    Added svg-images in css.
    Changed some color values (more color-variables).
    Changed background-image because it looks better.
    Added marker-svg's to the input-fields, so that the players know who
    plays with x and who plays with o.
    Changed selector "message-panel" to "game-info" because inside of
    "game-info" is now a message-panel and a marker-panel.

--- script.js
+++ script.js
@@ -46,7 +46,9 @@ function gameUserInterface(gameBoard) {
     const gameUi = document.querySelector(".game-interface");
     const formUi = document.querySelector(".form-ui");
     const boardUi = document.querySelector(".board-ui");
-    const messagePanel = boardUi.querySelector(".message-panel");
+    const gameInfo = document.querySelector(".game-info");
+    const messagePanel = gameInfo.querySelector("p");
+    const markerPanel = gameInfo.querySelector("span");
     const board = boardUi.querySelector(".board");
 
     function getGameUi() {
@@ -73,28 +75,36 @@ function gameUserInterface(gameBoard) {
         }
     }
 
-    function updateMessagePanel(activePlayer, message) {
-        switch (message) {
-            case "playerTurn":
-                messagePanel.classList.remove("win");
-                messagePanel.classList.remove("tie");
-                messagePanel.textContent = `${activePlayer.name}'s Turn. (${activePlayer.marker})`;
-                break;
-            case "win":
-                messagePanel.classList.add("win");
-                messagePanel.textContent = `Congratulations! ${activePlayer.name} wins!`;
-                break;
-            case "tie":
-                messagePanel.classList.add("tie");
-                messagePanel.textContent = "The game is over! It's a tie!"
-                break;
-        }
-    }
-
     function updateBoard() {
         gameBoard.getBoard().flat().map((element, index) => {
-            board.children[index].textContent = element;
+            if (element === "") {
+                board.children[index].classList.remove("x-marker", "o-marker");
+            } else {
+                board.children[index].classList.add(`${element}-marker`);
+            }
         });
+    }
+
+    function updateMessagePanel(activePlayer, message) {
+        markerPanel.classList.remove("x-marker", "o-marker");
+        gameInfo.classList.remove("win", "tie");
+        switch (message) {
+            case "playerTurn":
+                messagePanel.textContent = `${activePlayer.name}'s turn:`;
+                markerPanel.classList.add("panel-marker");
+                markerPanel.classList.add(`${activePlayer.marker}-marker`);
+                break;
+            case "win":
+                markerPanel.classList.remove("panel-marker");
+                messagePanel.textContent = `Congratulations! ${activePlayer.name} wins!`;
+                gameInfo.classList.add("win");
+                break;
+            case "tie":
+                markerPanel.classList.remove("panel-marker");
+                messagePanel.textContent = "The game is over! It's a tie!"
+                gameInfo.classList.add("tie");
+                break;
+        }
     }
 
     return {getGameUi, render};
@@ -104,18 +114,15 @@ function GameController(playerOneName = "Player 1", playerTwoName = "Player 2") 
     const gameBoard = Gameboard();
     const gameUi = gameUserInterface(gameBoard);
     const players = [
-        {name: playerOneName, marker: "X"},
-        {name: playerTwoName, marker: "O"}
+        {name: playerOneName, marker: "x"},
+        {name: playerTwoName, marker: "o"}
     ]
 
     let activePlayer = players[0];
     
     gameUi.getGameUi().querySelector(".form-ui").addEventListener("submit", (e) => submitForm(e));
-
     gameUi.getGameUi().querySelector(".board").addEventListener("click", (e) => startGameCycle(e));
-
     gameUi.getGameUi().querySelector(".restart-game").addEventListener("click", () => restartGame());
-
     gameUi.getGameUi().querySelector(".new-game").addEventListener("click", () => startNewGame());
 
     function submitForm(e) {
@@ -157,7 +164,7 @@ function GameController(playerOneName = "Player 1", playerTwoName = "Player 2") 
         const cell = {
             row: e.target.getAttribute("data-index-numbers").at(0),
             column: e.target.getAttribute("data-index-numbers").at(-1)
-            };
+        };
         if (gameBoard.getBoard()[cell.row][cell.column] === "" && !isWin()) {
             gameBoard.placeMarker(cell, activePlayer);
             let message;
